474360fed92358d55f71681c6fba9ae3663c3aa4
RivaGiorgia <riva.giorgia@itisgrassi.edu.it>
2025-02-07 12:01:49 +0100

    aggiunto esercizio1

---
+++ Esercizio1.java
@@ -0,0 +1,21 @@
+//1
+import java.util.Scanner; 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci un numero: ");
+        int num = scanner.nextInt();
+        if (num % 2) { 
+           int num1 = num/2
+            System.out.println(num1);
+        }
+ else if (num % 3) { 
+            int num2 = num*3-1
+            System.out.println("num2");
+        }
+ else {    
+           System.out.println("numero non valido"); 
+        }
+    }
+}
+
